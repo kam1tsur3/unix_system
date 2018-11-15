d11ed68d6bddeef7ac9b191d3310897eeea5b477
kam1tsur3 <cloudyscrafty@icloud.com>
2018-11-15 15:56:49 +0900

    report3

--- report3/main.c
+++ report3/main.c
@@ -1,32 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define BUF_SIZE 	100
-void main(int argc, char** argv)
+#define ARG_SIZE	50
+
+int main(int argc, char** argv)
 {
 	char buf[BUF_SIZE];
-	int p_id;
+	int i, p_id;
 	char pipe_flag;
-	char** p_argv;
+	char* p_argv[ARG_SIZE];
+	char* tmp_addr;
+	int *status;
 
 	while(1)
 	{
-		puts("$");
-		fgets(buf, BUF_SIZE, stdin);
-
-
-
-		p_id = fork();
-		if(p_id < 0){
-			perror("fork");
-			exit(1);
+		putchar('$');
+		memset(buf, 0, BUF_SIZE);
+		for(i = 0; i < ARG_SIZE; i++)
+			p_argv[i] = NULL;
+		fgets(buf, BUF_SIZE - 1, stdin);
+			
+		p_argv[0] = buf;
+		tmp_addr = strchr(buf, '\n');
+		if(tmp_addr != NULL)
+			*tmp_addr = 0;
+		
+		for(i = 1, tmp_addr = buf; i < ARG_SIZE - 1; i++){
+			tmp_addr = strchr(tmp_addr, ' ');
+			if(tmp_addr == NULL)break;
+			*tmp_addr = '\x00';
+			tmp_addr++;
+			if(*tmp_addr == ' ' || *tmp_addr == '\x00'){
+				i--;
+				continue;
+			}
+			p_argv[i] = tmp_addr;
 		}
-		else if(p_id == 0){
 
+		if(!strcmp(p_argv[0], "cd")){
+			chdir(p_argv[1]);
+			continue;
+		}
+		else if(!strcmp(p_argv[0], "exit")){
+			exit(0);
 		}
 		else{
-
+			p_id = fork();
+			if(p_id < 0){
+				perror("fork");
+				exit(1);
+			}
+			else if(p_id == 0){
+				execvp(p_argv[0], &p_argv[0]);
+				exit(0);
+			}
+			else{
+				wait(status);
+			}
 		}
 	}
 }
