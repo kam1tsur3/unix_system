d3ee94e9ab380bc6293e7c356415ce2fb0d34449
kam1tsur3 <cloudyscrafty@icloud.com>
2018-11-24 01:15:33 +0900

    main.c

--- report3/main.c
+++ report3/main.c
@@ -2,9 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #define BUF_SIZE 	100
 #define ARG_SIZE	50
+
+void analysys_args(char** p_argv, char* buf){
+	
+	char* tmp_addr;
+	int i;
+
+	p_argv[0] = buf;
+	tmp_addr = strchr(buf, '\n');
+	if(tmp_addr != NULL)
+		*tmp_addr = 0;
+	
+	for(i = 1, tmp_addr = buf; i < ARG_SIZE - 1; i++){
+		tmp_addr = strchr(tmp_addr, ' ');
+		if(tmp_addr == NULL)break;
+		*tmp_addr = '\x00';
+		tmp_addr++;
+		if(*tmp_addr == ' '){
+			i--;
+			continue;
+		}
+		p_argv[i] = tmp_addr;
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -23,22 +47,7 @@ int main(int argc, char** argv)
 			p_argv[i] = NULL;
 		fgets(buf, BUF_SIZE - 1, stdin);
 			
-		p_argv[0] = buf;
-		tmp_addr = strchr(buf, '\n');
-		if(tmp_addr != NULL)
-			*tmp_addr = 0;
-		
-		for(i = 1, tmp_addr = buf; i < ARG_SIZE - 1; i++){
-			tmp_addr = strchr(tmp_addr, ' ');
-			if(tmp_addr == NULL)break;
-			*tmp_addr = '\x00';
-			tmp_addr++;
-			if(*tmp_addr == ' ' || *tmp_addr == '\x00'){
-				i--;
-				continue;
-			}
-			p_argv[i] = tmp_addr;
-		}
+		analysys_args(p_argv, buf);
 
 		if(!strcmp(p_argv[0], "cd")){
 			chdir(p_argv[1]);
@@ -58,8 +67,36 @@ int main(int argc, char** argv)
 				exit(0);
 			}
 			else{
-				wait(status);
+				if(wait(status) == -1){
+					perror("wait");
+					exit(1);
+				}
 			}
 		}
 	}
 }
+/*
+void newpro(int argc, char** argv)
+{
+	pid_t pid;
+	
+	if(){//argv has special token
+		if((pid = fork()) < 0){
+			perror("fork");
+			exit(1);
+		}
+		else if(pid == 0){
+			newpro();	
+		}
+		else{
+			//exec
+			wait();
+			exit(0);
+		}
+	}
+	else{
+		//exec
+		exit(0);
+	}
+}
+*/
